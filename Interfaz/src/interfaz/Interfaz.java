bf898705e9491e21177095b1e36e15f887485657
Gael <gaelmaldonado867@gmail.com>
2023-02-24 20:21:14 -0600

    Segunda Carga

--- Interfaz/src/interfaz/Interfaz.java
+++ Interfaz/src/interfaz/Interfaz.java
@@ -12,9 +12,12 @@ public class Interfaz {
 
     /**
      * @param args the command line arguments
-     */
+     */ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // jejeje
     }
+     
+      
+    
     
 }
